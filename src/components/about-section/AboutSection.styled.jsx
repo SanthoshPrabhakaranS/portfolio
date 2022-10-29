98fefd868db2ac194bd8fadedc18f0de883cc703
Santhosh Prahakaran <santhoshprabhakaran02@gmail.com>
2022-10-29 15:57:59 +0530

    about height fixed.

--- src/components/about-section/AboutSection.styled.jsx
+++ src/components/about-section/AboutSection.styled.jsx
@@ -9,7 +9,6 @@ export const Container = styled(Box)(({ theme }) => ({
   justifyContent: "center",
   alignItems: "Center",
   flexDirection: "column",
-  
 }));
 
 export const TextContainer = styled(Box)(({ theme }) => ({
