40054fb8f5043c6532c5dc3881c8990804c8ed31
Santhosh Prabhakaran <santhoshprabhakaran02@gmail.com>
2022-11-04 18:23:06 +0530

    final changes

--- src/theme/theme.jsx
+++ src/theme/theme.jsx
@@ -1,23 +1,9 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-    // palette: {
-    //     primary: {
-    //         main: "#64ffda",
-    //     },
-    //     text: {
-    //         main: "#ccd6f6",
-    //         light: "#8892b0",
-    //         dark: "#5f6a84"
-    //     },
-    //     background:{
-    //         main: "#0a192f",
-    //         light: "#112240"
-    //     }
-    // }
     palette: {
         primary: {
-            main: "#64ffda",
+            main: "#1dd05d",
         },
         text: {
             main: "#ccd6f6",
