d376c40640ac614e948dc2459db7853a6aec3524
Santhosh Prabhakaran <santhoshprabhakaran02@gmail.com>
2022-11-14 11:17:56 +0530

    Final

--- src/components/hero-section/index.jsx
+++ src/components/hero-section/index.jsx
@@ -5,19 +5,19 @@ import { Container, TextContainer } from "./HeroSection.styled";
 const HeroSection = () => {
   return (
     <Container id="/">
-        <TextContainer>
-          <p>Hi, my name is</p>
-          <p>Santhosh Prabhakaran.</p>
-          <p>I build things for the web.</p>
-          <p>
-            I'm a Frontend-developer specializing in building exceptional
-            digital experiences. Currently, I'm focused on building accessible,
-            blockchain products at Ionixx Texhnoligoes.
-          </p>
-          <a href="#about">
-            <Button>Know More About Me.</Button>
-          </a>
-        </TextContainer>
+      <TextContainer>
+        <p>Hi, my name is</p>
+        <p>Santhosh Prabhakaran.</p>
+        <p>I build things for the web.</p>
+        <p>
+          I'm a Frontend-developer specializing in building exceptional digital
+          experiences. Currently, I'm focused on building accessible, blockchain
+          products at Ionixx Technologies.
+        </p>
+        <a href="#about">
+          <Button>Know More About Me.</Button>
+        </a>
+      </TextContainer>
     </Container>
   );
 };
